c561942092ea914dba53a08ca49583167a166e05
MaciejWodnicki <wodnicki.maciej@gmail.com>
2022-03-23 13:00:40 +0100

    Poprawione kolizje z bokami platform. Postać obraca się teraz w kierunku ruchu

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,12 +15,13 @@ namespace WindowsFormsApp1
     {
         Graphics g;
         List<Rectangle> PlatformHB = new List<Rectangle>(); // hitboxy platform
-        Rectangle Player = new Rectangle(80, 200, Properties.Resources.Chungus.Width, Properties.Resources.Chungus.Height);
+        Rectangle Player = new Rectangle(80, 200, Properties.Resources.Chungus.Width, Properties.Resources.Chungus.Height); //hitbox postaci
+        Bitmap PlayerBitmap;
         bool playerRight = false;
         bool playerLeft = false;
         bool playerUp = false;
-        bool isGrounded = false;
-        bool collisionWithSide = false;
+        bool isGrounded = false; // sprawdza czy gracz dotyka ziemi
+        bool faceLeft = false; // zapamietuje ostatni kierunek ruchu
         int fallSpeed = 0;
         int jumpSpeed = 200;
         int playerSpeed = 5;
@@ -30,6 +31,7 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
+            
             WindowState = FormWindowState.Maximized;
         }
 
@@ -58,10 +60,12 @@ namespace WindowsFormsApp1
             if (e.KeyCode == Keys.Left)
             {
                 playerLeft = true;
+                faceLeft = true;
             }
             if (e.KeyCode == Keys.Right)
             {
                 playerRight = true;
+                faceLeft=false;
             }
             if (e.KeyCode == Keys.Up && isGrounded)
             {
@@ -72,11 +76,11 @@ namespace WindowsFormsApp1
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (playerLeft == true && !collisionWithSide)
+            if (playerLeft == true)
             {
                 Player.X -= playerSpeed;
             }
-            if (playerRight == true && Player.X < this.Width && !collisionWithSide)
+            if (playerRight == true && Player.X < this.Width)
             {
                 Player.X += playerSpeed;
             }
@@ -84,7 +88,7 @@ namespace WindowsFormsApp1
             {
                 Player.Y -= jumpSpeed / 10;
                 if (jumpSpeed > 0)
-                    jumpSpeed -= 7;
+                    jumpSpeed -= 11;
                 else if (jumpSpeed <= 0)
                 {
                     playerUp = false;
@@ -96,7 +100,7 @@ namespace WindowsFormsApp1
             {
                 Player.Y += fallSpeed / 10;
                 if (fallSpeed < 100)
-                    fallSpeed += 7;
+                    fallSpeed += 9;
                 isGrounded = false;
             }
 
@@ -104,40 +108,31 @@ namespace WindowsFormsApp1
             {
                 // kolizja gracza z gorna krawedzia zatrzymuje opadanie //dziala
                 if (Player.Y+Player.Height < hb.Y+20 && Player.Y+Player.Height > hb.Y-4 &&
-                    Player.X < hb.X + hb.Width && Player.X > hb.X)
+                    Player.X < hb.X + hb.Width+50 && Player.X > hb.X-50)
                 {
                     fallSpeed = 0;
                     jumpSpeed = 200;
                     isGrounded = true;
                 }
                 //kolizja z dolna krawedzia zatrzymuje skok // dziala
-                if (Player.Y < hb.Y + hb.Height + 50 && Player.Y > hb.Y + hb.Height &&
+                if (Player.Y < hb.Y + hb.Height + 40 && Player.Y > hb.Y + hb.Height &&
                     Player.X < hb.X + hb.Width && Player.X > hb.X)
                 {
                     jumpSpeed = 0;
-                    //fallSpeed = 30;
                 }
-                //kolizja z bocznymi krawedziami // wyklada sie na warunku Y
-                if (((Player.Y + Player.Height > hb.Y + hb.Height -5    &&    Player.Y + Player.Height< hb.Y +5) &&             //warunek Y // do poprawy
-                          (( Player.X+Player.Width > hb.X -5     &&         Player.X + Player.Width < hb.X + 5)||         //warunek X z lewej
-                           (Player.X > hb.X+hb.Width-5           &&         Player.X < hb.X+hb.Width+5))  //warunek X z prawej
-                            
-                    ))
-                {
-                    //playerSpeed = 0;
-                    collisionWithSide = true;
-                    //fallSpeed = 30;
-                }
-                else
-                    collisionWithSide = false;
 
+                //kolizja z bocznymi krawedziami 
+                if (Player.Contains(hb.X,hb.Y) || Player.Contains(hb.X+hb.Width, hb.Y + hb.Height))
+                {
+                        Player.Y--;
+                }
 
             }
 
+
+
             pictureBox1.Image = new Bitmap(this.Width, this.Height);
             g = Graphics.FromImage(pictureBox1.Image);
-
-
             generatePlatform(0, Height - 100, Width);
 
             generatePlatform(Width / 10, Height / 3, Width / 5);
@@ -145,7 +140,15 @@ namespace WindowsFormsApp1
             generatePlatform(3 * Width / 5, 2 * Height / 5, Width / 20);
             generatePlatform(Width / 3, Height / 10, Width / 20);
 
-            g.DrawImage(Properties.Resources.Chungus, Player.X, Player.Y);
+            PlayerBitmap = Properties.Resources.Chungus;
+
+            if (faceLeft) // zmienia zwrot postaci
+            {
+                PlayerBitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+            }
+ 
+
+            g.DrawImage(PlayerBitmap, Player.X, Player.Y);
             pictureBox1.Refresh();
 
         }
@@ -176,11 +179,6 @@ namespace WindowsFormsApp1
                 int ran3 = rand.Next(100, 200);
                 generatePlatform(ran1, ran2, ran3);
             }
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
